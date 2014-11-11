25f144e7369bde63c7a9f9a0d399f875f5f96bbc
Aleh Kot <kot.oleg@gmail.com>
2014-11-11 18:14:13 +0300

    Use k0teg.me domain to fetch data.

--- js/drupal.js
+++ js/drupal.js
@@ -1,19 +1,13 @@
 (function($){
-  $.getJSON( "https://pipes.yahoo.com/pipes/pipe.run?_id=0506658508c5f3b8ed372cf9b835859e&_render=json", function( data ) {
-    var items = [];
-    if (data.count) {
-      var item_title;
-      var $content = $('div[data-role="content"]');
-      var $new_fieldset;
-      $.each( data.value.items, function( key, val ) {
-        $val = $(val.content);
-        item_title = $val.find('h2 a').html();
-        $new_fieldset = $content.append('<fieldset data-role="collapsible"><legend>' + item_title + '</legend><div>' + $val.html() + '</div></fieldset>')
-          .enhanceWithin();
-        $new_fieldset.find('.field-name-body').siblings().remove();
-        $new_fieldset.find('h2').first().remove();
+  $.getJSON( "http://k0teg.me/drupalorg_modules", function( data ) {
+    if (!!data.length) {
+      var $content = $('div[data-role="content"]'), text, title;
+      $.each(data, function(key, val){
+        text = $("<div/>").html(val.content).text();
+        title = val.name;               
+        $new_fieldset = $content.append('<fieldset data-role="collapsible"><legend>' + title + '</legend><div>' + text + '</div></fieldset>')
+          .enhanceWithin();      
       });
     }
-    $('.jquery-ui-loader').hide();
   });
 })(jQuery);
